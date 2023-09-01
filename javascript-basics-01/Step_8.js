3c5b678509a5c4e81fe40bbc46ebae0c86dee997
Rachwan <rachwan.harb2023@gmail.com>
2023-09-01 20:54:15 +0300

    Completed Step 8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+var userAge = parseInt(prompt("Your age:"));
+
+if (userAge > 18) {
+    alert("You are over 18.");
+} else if(userAge < 18) {
+    alert("You are under 18.");
+} else {
+    alert("You are 18!")
+}
