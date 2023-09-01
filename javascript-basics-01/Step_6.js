b34d8e33c7334e53233acac2971163b9d6c7ce76
Rachwan <rachwan.harb2023@gmail.com>
2023-09-01 20:30:57 +0300

    Completed Step 6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,5 @@
+var num1 = parseFloat(prompt("Enter your first number:"));
+var num2 = parseFloat(prompt("Enter your second number:"));
+var quotient = num1 / num2;
+var remainder = num1 % num2;
+alert("Quotient: " + quotient + "\n" + "Remainder: " + remainder);
