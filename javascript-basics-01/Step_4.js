aa346c9162e166645b6f506310536ced807d44dd
Rachwan <rachwan.harb2023@gmail.com>
2023-09-01 20:11:48 +0300

    Completed Step 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("Please enter your name:");
+var surname = prompt("Please enter your surname:");
+var city = prompt("Please enter your city:");
+var message = "Name: " + name + "\n" + "Surname: " + surname + "\n" + "City: " + city;
+alert(message);
